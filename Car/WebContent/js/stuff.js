d52d9b81f1b482ee587932a8c30c04b49dcbf978
Jing <Jing@10.0.0.142>
2017-07-15 01:12:00 -0400

    7_15_17_Commit1

---
+++ Car/WebContent/js/stuff.js
@@ -0,0 +1,49 @@
+var CarApp = angular.module('CarApp', []);
+
+/*CarApp.controller('getCar', function($scope, $http) {
+
+	$scope.color = {
+		singleSelect : null,
+		option1 : 'option-1'
+	};
+
+	$http.get('json/car.json').success(function(response) {
+		$scope.getCar = response;
+
+	})
+
+});*/
+
+CarApp
+		.factory(
+				'getCar',
+				function($http) {
+					var factory = [];
+
+					factory.getCarDetails = function() {
+						return $http
+								.get("xml/response.xml");
+					}
+
+					return factory;
+				});
+
+CarApp.controller('car', function($scope, getCar) {
+
+	$scope.color = {
+		singleSelect : null,
+		option1 : 'option-1'
+	};
+	
+	$scope.carDetail = [];
+	loadCar();
+
+	function loadCar() {
+		var x2js = new X2JS();
+		getCar.getCarDetails().success(function(data) {
+			carDetail = x2js.xml_str2json(data);
+			console.log(carDetail.response.car);
+			$scope.carDetail = carDetail.response.car;
+		});
+	}
+});
