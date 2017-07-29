ae2ff6daa07fb94ad58ef6bf66e608b56be530a3
Jing <Jing@10.0.0.142>
2017-07-28 23:54:00 -0400

    7_28_2017Commit3

--- Car/WebContent/js/stuff.js
+++ Car/WebContent/js/stuff.js
@@ -1,92 +1,72 @@
 var CarApp = angular.module('CarApp', []);
 
-
-
 /*
  * Get car response
  */
 
-CarApp
-.factory(
-		'GetCar',
-		function($http) {
-			var factory = [];
+CarApp.factory('GetCar', function($http) {
+	var factory = [];
 
-			factory.getCarDetails = function() {
-				return $http
-						.get("xml/getCarResponse.xml");
-			}
+	factory.getCarDetails = function() {
+		return $http.get("xml/getCarResponse.xml");
+	}
 
-			return factory;
-		});
-
+	return factory;
+});
 
 /*
  * Create car response
  */
-CarApp
-.factory(
-		'CreateCar',
-		function($http) {
-			var factory = [];
 
-			factory.createCarResult = function() {
-				return $http
-						.get("xml/CreateCarsResponse.xml");
-			}
+CarApp.factory('CreateCar', function($http) {
+	var factory = [];
 
-			return factory;
-		});
+	factory.responseMsg = function() {
+		return $http.get("xml/CreateCarsResponse.xml");
+	}
 
+	return factory;
+});
 
-
-CarApp.controller('Car', function($scope, $http,GetCar,CreateCar) {
+CarApp.controller('Car', function($scope, $http, GetCar, CreateCar) {
 
 	$scope.color = {
 		singleSelect : null,
 		option1 : 'option-1'
 	};
 
-/*	$http.get('json/car.json').success(function(response) {
+	$http.get('json/car.json').success(function(response) {
 		$scope.getCar = response;
 
 	})
-	
+
 	$http.get('json/createCar.json').success(function(response) {
 		$scope.createCar = response;
 
-	})*/
-	
-	
-	
-	
-	$scope.carDetails = [];
-	$scope.createCarResponse = [];
-	car();
+	})
 
+	$scope.carDetails = [];
+	$scope.createCars;
 	
+	
+
+	car();
+	
+
 	function car() {
 		var x2js = new X2JS();
+
 		GetCar.getCarDetails().success(function(response) {
 			carDetail = x2js.xml_str2json(response);
 			console.log(carDetail.response.car);
 			$scope.carDetails = carDetail.response.car;
 		});
 		
-		
-		CreateCar.createCarResult().success(function(response) {
-			createCarResults = x2js.xml_str2json(response);
-			console.log(createCarResults.response);
-			$scope.createCarResponse = createCarResults.response;
+		CreateCar.responseMsg().success(function(response) {
+			createcar = x2js.xml_str2json(response);
+			console.log(createcar.response);
+			$scope.createCars = createcar.response;
 		});
 	}
-	
-	
 
 });
-
-
-
-
-
-
