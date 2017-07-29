cae10fcc3daf87ad524e6ed86fc89833abe1edb1
Jing <Jing@10.0.0.142>
2017-07-28 22:52:17 -0400

    7_28_2017commit1

--- Car/WebContent/js/stuff.js
+++ Car/WebContent/js/stuff.js
@@ -1,27 +1,53 @@
 var CarApp = angular.module('CarApp', []);
 
+
+
+/*
+ * Get car response
+ */
+
 CarApp
 .factory(
-		'Car',
+		'GetCar',
 		function($http) {
 			var factory = [];
 
 			factory.getCarDetails = function() {
 				return $http
-						.get("xml/response.xml");
+						.get("xml/getCarResponse.xml");
 			}
 
 			return factory;
 		});
 
-CarApp.controller('getCar', function($scope, $http,Car) {
+
+/*
+ * Create car response
+ */
+CarApp
+.factory(
+		'CreateCar',
+		function($http) {
+			var factory = [];
+
+			factory.createCarResult = function() {
+				return $http
+						.get("xml/CreateCarsResponse.xml");
+			}
+
+			return factory;
+		});
+
+
+
+CarApp.controller('Car', function($scope, $http,GetCar,CreateCar) {
 
 	$scope.color = {
 		singleSelect : null,
 		option1 : 'option-1'
 	};
 
-	$http.get('json/car.json').success(function(response) {
+/*	$http.get('json/car.json').success(function(response) {
 		$scope.getCar = response;
 
 	})
@@ -29,51 +55,33 @@ CarApp.controller('getCar', function($scope, $http,Car) {
 	$http.get('json/createCar.json').success(function(response) {
 		$scope.createCar = response;
 
-	})
+	})*/
 	
 	
 	
 	
 	$scope.carDetails = [];
-	loadCar();
+	$scope.createCarResponse = [];
+	car();
 
-	function loadCar() {
+	
+	function car() {
 		var x2js = new X2JS();
-		Car.getCarDetails().success(function(response) {
+		GetCar.getCarDetails().success(function(response) {
 			carDetail = x2js.xml_str2json(response);
-		/*	console.log(carDetail.Envelope.Header.Body.GetCarsResponse.car);
-			$scope.carDetails = carDetail.Envelope.Header.Body.GetCarsResponse.car;*/
-			
 			console.log(carDetail.response.car);
 			$scope.carDetails = carDetail.response.car;
 		});
+		
+		
+		CreateCar.createCarResult().success(function(response) {
+			createCarResults = x2js.xml_str2json(response);
+			console.log(createCarResults.response);
+			$scope.createCarResponse = createCarResults.response;
+		});
 	}
 	
-/*
-	$(window)
-			.load(
-					function() {
-						var x2js = new X2JS();
-						function convertXml2JSon() {
-							$("#jsonArea").val(
-									JSON.stringify(x2js.xml_str2json($(
-											"#xmlArea").val())));
-						}
-
-						function convertJSon2XML() {
-							$("#xmlArea").val(
-									x2js.json2xml_str($
-											.parseJSON($("#jsonArea").val())));
-						}
-
-						$("#xmlArea")
-								.val(
-										"<response><car><name>SUBARU LEGACY</name><color>BLUE</color><fuelAmount>0</fuelAmount><carType>GAS</carType></car><car><name>TOYOTA CAMRY</name><color>WHITE</color><fuelAmount>10</fuelAmount><carType>GAS</carType></car></response>");
-						convertXml2JSon();
-						convertJSon2XML();
-						$("#convertToJsonBtn").click(convertXml2JSon);
-						$("#convertToXmlBtn").click(convertJSon2XML);
-					});*/
+	
 
 });
 
